125c4d81fa1490106f439765f94c52e00ecb4c90
Chris Loeper <chrisloeper@gmail.com>
2020-05-04 16:35:19 -0700

    completes refactor of CSS and hexes

--- src/components/Hex.tsx
+++ src/components/Hex.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import goldenHex from '../images/goldenhex.png';
+import * as goldenHex from '../images/goldenhex.png';
 import styled from 'styled-components';
-import { transformObjectKeys } from '../utils/objectUtils';
 
 interface IHexPositions {
   layerOne: IHexLayer;
@@ -25,17 +24,17 @@ const hexPositions: IHexPositions = {
   layerOne: {
     left: { top: 217, left: 0 },
     middle: { top: 217, left: 113 },
-    right: { top: 217, left: 330 },
+    right: { top: 217, left: 225 },
   },
   layerTwo: {
     left: { top: 157, left: 0 },
     middle: { top: 185, left: 59 },
-    right: { top: 185, left: 171 },
+    right: { top: 158, left: 223 },
   },
   layerThree: {
     left: { top: 155, left: -107 },
     middle: { top: 155, left: 111 },
-    right: { top: 127, left: 65 },
+    right: { top: 155, left: 117 },
   },
   layerFour: {
     left: { top: 137, left: -33 },
@@ -76,5 +75,3 @@ export const Hex = {
     Right: () => createHex(hexPositions.layerFour.right),
   },
 };
-
-//[a-zA-Z0-9-?!@#$%^&*()+ ]+(?![^:]*:)
---
+++ src/components/ParallaxLayers.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ParallaxContainer = styled.div`
+  perspective: 1px;
+  height: 100vh;
+  min-height: 1080px;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+export const ParallaxLayer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+export const ParallaxLayerBase = styled(ParallaxLayer)`
+  z-index: 99;
+  transform: translateZ(0);
+`;
+
+export const ParallaxLayerMenu = styled(ParallaxLayer)`
+  transform: translateZ(-6px) scale(7);
+  z-index: 100;
+`;
+
+export const ParallaxLayerOne = styled(ParallaxLayer)`
+  transform-origin: 0 0;
+  transform: translateZ(-1px) scale(3);
+  z-index: 1;
+  display: flex;
+`;
+
+export const ParallaxLayerTwo = styled(ParallaxLayer)`
+  transform-origin: 0 0;
+  transform: translateZ(-2px) scale(4.5);
+  z-index: 2;
+  display: flex;
+`;
+
+export const ParallaxLayerThree = styled(ParallaxLayer)`
+  transform-origin: 0 0;
+  transform: translateZ(-3px) scale(6);
+  z-index: 3;
+  display: flex;
+`;
+
+export const ParallaxLayerFour = styled(ParallaxLayer)`
+  transform-origin: 0 0;
+  transform: translateZ(-4px) scale(7.5);
+  z-index: 4;
+  display: flex;
+`;
